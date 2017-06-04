1dad612abfe1831749ab4c6d3f08b2b3ead2460a
peloton <j.peloton@sussex.ac.uk>
2017-06-04 22:16:08 +0100

    Replace time by Wtime for MPI usage

--- ProfileIt/communication.py
+++ ProfileIt/communication.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 verbose = False
 
@@ -9,6 +10,7 @@ try:
     size = MPI.COMM_WORLD.Get_size()
     barrier = MPI.COMM_WORLD.Barrier
     finalize = MPI.Finalize
+    Wtime = MPI.Wtime
     if verbose:
         print 'Parallel setup OK, rank %s in %s' % (rank, size)
 except:
@@ -17,5 +19,6 @@ except:
     size = 1
     barrier = lambda: -1
     finalize = lambda: -1
+    Wtime = time.time
     if verbose:
         print 'No mpi4py found - switching to serial mode'
