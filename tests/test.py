a63438da61da9209435510949a12ea39e3da6974
peloton <j.peloton@sussex.ac.uk>
2017-06-09 09:53:43 +0100

    update the test script

--- tests/test.py
+++ tests/test.py
@@ -1,38 +1,41 @@
+## Example script
 from InsideMe import profiler
 from InsideMe import communication as comm
 import numpy as np
 import os
+import string
+import random
 
-@profiler.benchmark(field='Generate data set')
-def generate_data(nmc, rank):
+@profiler.benchmark(field='Computation')
+def generate_data(nmc, size):
     """
-    Generate fake data set
+    Generate fake data set.
 
     Parameters
     -----------
         * nmc: int, number of MC simulations
-        * rank: int, the rank of the processor
+        * size: int, boost factor to expand the size of our vector
     """
-    rand_vec = np.zeros((nmc, nmc * (rank + 1)))
+    rand_vec = np.zeros((nmc, nmc * (size + 1)))
 
     for mc in range(nmc):
-        rand_vec[mc] = np.random.rand(nmc * (rank + 1))
+        rand_vec[mc] = np.random.rand(nmc * (size + 1))
 
     return rand_vec
 
 @profiler.benchmark(field='Computation')
-def accumulate_data(dataset, rank):
+def accumulate_data(dataset, size):
     """
-    Accumulate random vectors into a matrix
+    Accumulate random vectors into a matrix.
 
     Parameters
     -----------
-        * dataset: 2D array, (nmc, nmc * (rank + 1)) array containing your
-            measurements.
-        * rank: int, the rank of the processor
+        * dataset: 2D array, (nmc, nmc * (size + 1)) array
+            containing your measurements.
+        * size: int, boost factor to expand the size of our vector
     """
     nmc = dataset.shape[0]
-    mat = np.zeros((nmc * (rank + 1), nmc * (rank + 1)))
+    mat = np.zeros((nmc * (size + 1), nmc * (size + 1)))
 
     for mc in range(nmc):
         mat += np.outer(dataset[mc], dataset[mc])
@@ -42,7 +45,7 @@ def accumulate_data(dataset, rank):
 @profiler.benchmark(field='I/O')
 def write_on_disk(mat, fname):
     """
-    Write and delete files on disk
+    Write and delete files on disk.
 
     Parameters
     -----------
@@ -53,9 +56,33 @@ def write_on_disk(mat, fname):
     np.save(fname ,mat)
     os.remove(fname)
 
+@profiler.benchmark(field='Communication')
+def Reduce(mat, root):
+    """
+    Wrapper around reduce function.
+
+    Parameters
+    -----------
+        * mat: 2D array, the matrix that has to be sent to root
+        * root: int, rogue leader.
+    """
+    out = comm.comm.reduce(mat, op=comm.MPI.SUM, root=root)
+    return out
+
+## We decide to not monitor this one for example
+## It will be categorized in the <Others> field in plots.
+def random_generator(
+    size=6, chars=string.ascii_uppercase + string.digits):
+    """
+    Just for the fun.
+    """
+    return ''.join(random.choice(chars) for x in range(size))
+
 if __name__ == "__main__":
     for trial in range(20):
-        dataset = generate_data(nmc=10*trial, rank=comm.rank)
-        mat = accumulate_data(dataset, rank=comm.rank)
-        write_on_disk(mat, 'test_proc%d.npy' % (comm.rank))
+        dataset = generate_data(nmc=10*trial, size=comm.size)
+        mat = accumulate_data(dataset, size=comm.size)
+        mat = Reduce(mat, root=0)
+        if comm.rank == 0:
+            write_on_disk(mat, '%s.npy' % random_generator())
     comm.barrier()
